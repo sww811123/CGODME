9884a37eb8386fb6aedad71bd0acb31a44b3a1c6
Taehooie Kim <taehooie@urbansim.com>
2024-09-26 22:46:56 -0700

    Add tutorial and write README

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # CGODME
-This is an experimental version aimed at finding the optimal path flows through systematic mapping of variables, including zonal totals, OD flows, and link flows.
+This experimental version aims to find optimal path flows by systematically mapping variables (zonal totals, origin-destination demand, and link flows).
+
+## Quick Start
+Users can find Jupyter notebook that provides step-by-step instructions for utilization. 
+
+## Installation
+cgodme has been published on [PyPI](https://pypi.org/project/cgodme/) and can be installed using
+```
+$ pip install cgodme
+```
+
+## How to Cite
+- Journal article: [Kim, T., et al. Computational graph-based mathematical programming reformulation for integrated demand and supply models](https://www.sciencedirect.com/science/article/abs/pii/S0968090X2400192X)
 
--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="cgodme",
-    version="0.1.1",
-    description="Recalibrate Path Flows to Achieve Target OD Flow Consistency",
+    version="0.1.3",
+    description="Find optimal path flows that map systematic variables (origin-destination demand and link flows) to ensure consistency",
     author="Taehooie Kim, Ph.D., Xin Wu, Ph.D., Han Zheng, Ph.D., Xuesong Zhou, Ph.D.",
-    author_email="tkim@azmag.gov, xin.wu@villanova.edu, hzheng73@asu.edu, xzhou74@asu.edu",
+    author_email="taehooie.kim@gmail.com, xin.wu@villanova.edu, hzheng73@asu.edu, xzhou74@asu.edu",
     packages=find_packages(),
+    long_description=long_description,
     include_package_data=True,
     package_data={
         'cgodme': ["config.yaml"]
